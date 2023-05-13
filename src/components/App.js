a23c4f72c7d22a40875cb3b6f529a976356bc102
vipul440 <82093511+vipul440@users.noreply.github.com>
2023-05-13 11:23:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,18 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+	        this.state={isClicked:false};
+	        this.handleClick=this.handleClick.bind(this);
 	};
+	handleClick(){
+	 this.setState({isClicked:true});
+	}
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+		 <button id="click" onClick={this.handleClick}>Click me! </button>
+                  {this.state.isClicked && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     		</div>
     	);
     }
